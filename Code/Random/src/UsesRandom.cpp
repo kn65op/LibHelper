2989dec79424b16cc8fc96baf528dac4569cb257
kn65op <kn65op@gmail.com>
2015-03-16 20:53:27 +0100

    Removed useless helpers and change build system

--- Code/Random/src/UsesRandom.cpp
+++ Code/Random/src/UsesRandom.cpp
@@ -1,11 +1,4 @@
-/* 
- * File:   UsesRandom.cpp
- * Author: tomko
- * 
- * Created on 25 lipiec 2012, 12:33
- */
-
-#include "../include/UsesRandom.h"
+#include "../inc/UsesRandom.h"
 
 using namespace THelper;
 
@@ -16,7 +9,7 @@ UsesRandom::UsesRandom()
   gen = new std::mt19937(rd());
 }
 
-UsesRandom::UsesRandom(const UsesRandom& orig)
+UsesRandom::UsesRandom(const UsesRandom&)
 {
   //new engine
   gen = new std::mt19937(rd());

a0387b187a0a9c778b39b34c409a296cb279b6a2
kn65op <kn65op@gmail.com>
2015-03-22 11:27:42 +0100

    Use unique_ptr

--- Code/Random/inc/UsesRandom.h
+++ Code/Random/inc/UsesRandom.h
@@ -1,21 +1,14 @@
-/* 
- * File:   UsesRandom.h
- * Author: tomko
- *
- * Created on 25 lipiec 2012, 12:33
- */
-
-#ifndef USESRANDOM_H
-#define	USESRANDOM_H
+#pragma once
 
 #include <random>
+#include <memory>
 
 namespace THelper
 {
 
 /**
  * @brief Class which is interface to class that use random engine.
- * 
+ *
  * For now it use mersenne twister engine.
  * This class can be use to skip part of making engine and device in C++11 way. You can just create distribution which you like to use and use gen which is poiter to std::mt199937 engine.
  */
@@ -41,9 +34,7 @@ protected:
   static std::random_device rd;
   /** random engine
    */
-  std::mt19937 *gen;
+  std::unique_ptr<std::mt19937> gen;
 };
 
 }//namespace
-#endif	/* USESRANDOM_H */
-
--- Code/Random/src/UsesRandom.cpp
+++ Code/Random/src/UsesRandom.cpp
@@ -6,16 +6,16 @@ std::random_device UsesRandom::rd;
 
 UsesRandom::UsesRandom()
 {
-  gen = new std::mt19937(rd());
+//  gen = new std::mt19937(rd());
+  gen = std::make_unique<std::mt19937>(rd());
 }
 
 UsesRandom::UsesRandom(const UsesRandom&)
 {
   //new engine
-  gen = new std::mt19937(rd());
+  gen = std::make_unique<std::mt19937>(rd());
 }
 
 UsesRandom::~UsesRandom()
 {
-  delete gen;
 }

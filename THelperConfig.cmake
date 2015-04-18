b3289aa603d8f6c846827fe052fddab6d5f2c29b
kn65op <kn65op@gmail.com>
2015-04-18 18:13:28 +0200

    Add Config.cmake

---
+++ THelperConfig.cmake
@@ -0,0 +1,15 @@
+# - Config file for the THelper package
+# It defines the following variables
+#  THELPER_INCLUDE_DIRS - include directories for THelper
+#  THELPER_LIBRARIES    - libraries to link against
+#  THELPER_LIBRARIES_DIR- diretory to libraries to link against
+#  THELPER_EXECUTABLE   - the bar executable
+ 
+# Compute paths
+get_filename_component(THELPER_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(THELPER_INCLUDE_DIRS THelper/Code/THelper/inc)
+ 
+# These are IMPORTED targets created by THelperTargets.cmake
+set(THELPER_LIBRARIES_DIR THelper/${CMAKE_BUILD_TYPE})
+set(THELPER_LIBRARIES libTHelper.a)
+set(THELPER_TARGET THelper)
